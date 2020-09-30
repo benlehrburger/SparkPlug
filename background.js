a675cc684389c88e1bd3dd5cf583d0cbe05b215f
Ben Lehrburger <67259394+benlehrburger@users.noreply.github.com>
2020-09-30 15:57:00 -0400

    Update background.js

--- background.js
+++ background.js
@@ -5,10 +5,10 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
           pageUrl: {hostEquals: 'developer.chrome.com'},
@@ -17,5 +17,4 @@ chrome.runtime.onInstalled.addListener(function() {
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
-  });
 });
